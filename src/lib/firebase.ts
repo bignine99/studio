7096c79725d6a6b808fa185b5480ab3bd7747d2d
Danny Cho <bignine99@gmail.com>
2025-07-17 05:41:44 +0000

    웹 미리보기가 진행되지 않는다.. 무엇인가.. 큰 문제가 있는거 같은데.. 지난 파일을 붙여 넣었기 때문에.. 웹 이름이 바뀐 문

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-kQodxj4m-19Iqf76Z-9d0nx04OPc1RM",
-  authDomain: "accidentdb01.firebaseapp.com",
-  projectId: "accidentdb01",
-  storageBucket: "accidentdb01.firebasestorage.app",
-  messagingSenderId: "866936542603",
-  appId: "1:866936542603:web:acae9f165be86681e68b8e",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
